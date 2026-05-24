11eecf5fd47330f9589ae9caabab9d4fa0acacc4
hmarroquing5 <hmarroquing5@miumg.edu.gt>
2026-05-23 20:34:06 -0600

    Update App.jsx
    Se elimina import de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MenuPrincipal from './pages/MenuPrincipal';
